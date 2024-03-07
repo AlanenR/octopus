29055b74540855c3a86329962a77ee0a2ab1894b
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-03-07 06:42:07 +0200

    Add fetch courses

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import CardsContainer from "./containers/CardsContainer";
+import Slider from "./containers/Slider";
+import { useState, useEffect } from "react";
+
+const BASE_URL = "http://localhost:3001/courses";
+
+type Course = {
+  id: number;
+  title: string;
+  author: string;
+  description: string;
+  estimated_completion_time: number;
+  cover_image: string | null;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [courses, setCourses] = useState<Course[]>([]);
 
+  useEffect(() => {
+    const fetchCourses = async () => {
+      const response = await fetch(`${BASE_URL}`);
+      const data = await response.json();
+      setCourses(data as Course[]);
+    };
+
+    fetchCourses();
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <CardsContainer items={courses} />
+      <Slider />
+    </div>
+  );
 }
 
-export default App
+export default App;
