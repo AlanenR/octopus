8aff99116f2baae77fcc1ccf9f9d01effb6194b3
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-03-07 06:28:46 +0200

    Add courses route

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,18 @@
-import express from 'express';
+import express, { Express, Request, Response } from 'express'
+import coursesRoutes from './routes/courses'
+import dotenv from 'dotenv'
 
-const app = express();
-const PORT = 3001;
+dotenv.config()
 
-app.get('/', (req, res) => {
-  res.send('yo');
-});
+const app = express()
+const PORT = process.env.PORT
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Express + TypeScript Server')
+})
+
+app.use('/courses', coursesRoutes)
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
-});
+  console.log(`Server is running at http://localhost:${PORT}`)
+})
