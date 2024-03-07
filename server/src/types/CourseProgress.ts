d274fc80185dfd2251d7ff2bd43cb653d8880f63
Riikka Alanen <riikka.alanen@kuubi.fi>
2024-03-07 06:26:00 +0200

    Add types

---
+++ server/src/types/CourseProgress.ts
@@ -0,0 +1,6 @@
+export interface CourseProgress {
+  user_id: number
+  course_id: number
+  progress: number
+  completed: boolean
+}
---
+++ server/src/types/course.ts
@@ -0,0 +1,8 @@
+export interface Course {
+  id: number
+  title: string
+  author: string
+  description: string
+  estimated_completion_time: number
+  cover_image: string | null
+}
